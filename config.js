2b92c32097ab98508b6610b65d22700ed560d18a
27180781 <37129224+27180781@users.noreply.github.com>
2025-06-19 06:43:42 +0300

    config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 //          קובץ הגדרות מרכזי
 // =================================================
 // 1. החלף את הטקסט ב-App ID האמיתי שלך מאגורה
-const AGORA_APP_ID = "YOUR_AGORA_APP_ID";
+const AGORA_APP_ID = "517f71a5aa8a45a8a222341136c439c4";
 
 // 2. כדי להשתמש באבטחה, צור Token זמני באגורה עבור שם הערוץ: livegame_test_channel
 //    והדבק אותו כאן. אם תשאיר null, החיבור ייכשל אם הפרויקט שלך באגורה דורש Token.
-const AGORA_TOKEN = null; // דוגמה: "007eJxTY..."
+const AGORA_TOKEN = "007eJxTYCjTSZquu+dRz501iyzzjESPhyzYHbEmfsPlNytt2p5M5dyuwGBqaJ5mbphomphokWhiCiSMjIyMTQwNjc2STYwtk02E2oMzGgIZGWLeh7AyMkAgiC/KkJNZlpqemJsaX5JaXBKfnJGYl5eaw8AAAO47Jq4="
